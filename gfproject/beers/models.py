7585574f724c78fa483841399f69296400d3af35
Chris Betz <chris.betz@carecaminnovations.com>
2013-06-21 10:41:41 -0400

    inital data; views; routing

--- gfproject/beers/models.py
+++ gfproject/beers/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Brewery(models.Model):
     untappd_id = models.IntegerField()
     name = models.CharField(max_length=200)
-    created = models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
     country = models.CharField(max_length=100)
     logo = models.URLField(max_length=200)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -18,9 +18,9 @@ class Beer(models.Model):
     untappd_id = models.IntegerField()
     name = models.CharField(max_length=200)
     brewery = models.ForeignKey(Brewery)
+    label = models.URLField(max_length=200)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
-    label = models.URLField(max_length=200)
     
     def __unicode__(self):
     	return self.name
--- gfproject/beers/views.py
+++ gfproject/beers/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from beers.models import Brewery, Beer
+
+class IndexView(generic.ListView):
+    template_name = 'beers/index.html'
+    context_object_name = 'beer_list'
+
+    def get_queryset(self):
+        return Beer.objects.order_by('updated')
