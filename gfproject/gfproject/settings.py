bea5bba3643ac57b7b22a74b73c7d4c4af07ad21
Chris Betz <chris.betz@carecaminnovations.com>
2013-07-07 22:57:38 -0400

    heroku changes

--- gfproject/gfproject/settings.py
+++ gfproject/gfproject/settings.py
@@ -161,6 +161,13 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     "django.core.context_processors.request",
 )
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 try:
     from local_settings import *
 except ImportError:
